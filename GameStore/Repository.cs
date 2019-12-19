0a3065e60027d4690ef48b3af51e00f443779969
Александр Распопов <asraspopov@edu.hse.ru>
2019-12-19 18:05:50 +0300

    Добавил сериализацию

--- GameStore/Repository.cs
+++ GameStore/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using GameStore.Classes;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +13,42 @@ namespace GameStore
         public List<Game> Games;
         public List<Order> Orders;
         public List<Cashier> Cashiers;
+
+        public Repository()
+        {
+           
+        }
+
+        private T Deserialize<T>(string fileName)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                using (var jsonReader = new JsonTextReader(sr))
+                {
+                    var serializer = new JsonSerializer();
+                    return serializer.Deserialize<T>(jsonReader);
+                }
+            }
+        }
+
+        private void Serialize<T>(string fileName, T data)
+        {
+            using (var sw = new StreamWriter(fileName))
+            {
+                using (var jsonWriter = new JsonTextWriter(sw))
+                {
+                    jsonWriter.Formatting = Formatting.Indented;
+
+                    var serializer = new JsonSerializer();
+                    serializer.Serialize(jsonWriter, data);
+                }
+            }
+        }
+
+        private const string GamesFileName = "data/Games.json";
+        private const string TransactionsFileName = "data/Transactions.json";
+        private const string UsersFileName = "data/Clients.json";
+
 
     }
 }
