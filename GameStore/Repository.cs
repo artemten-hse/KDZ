29544aadda6e51a0460512774d28a94125920527
Александр Распопов <asraspopov@edu.hse.ru>
2019-12-19 17:55:54 +0300

    мелкие изменения

--- GameStore/Repository.cs
+++ GameStore/Repository.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using GameStore.Classes;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace GameStore
 {
     class Repository
     {
+        public List<Client> Clients;
+        public List<Game> Games;
+        public List<Order> Orders;
+        public List<Cashier> Cashiers;
+
     }
 }
